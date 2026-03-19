33c351b70baff77e0ff20e3fd386f17d07af082f
Germanldb <germanldb8@gmail.com>
2026-03-19 18:14:35 -0400

    chore: remove test/debug files and sync production code

--- test-cats.ts
+++
@@ -1,8 +0,0 @@
-import { wcFetch } from './src/lib/woocommerce';
-
-async function test() {
-    const cats = await wcFetch('/products/categories?per_page=100');
-    console.log(cats.map(c => ({ id: c.id, name: c.name, slug: c.slug })));
-}
-
-test();
