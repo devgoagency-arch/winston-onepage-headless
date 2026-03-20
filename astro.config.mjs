61d7f5a15824553f45d41cab5b5c2b3e471f1e78
German Devia <148661587+Germanldb@users.noreply.github.com>
2026-03-20 12:41:23 -0400

    Fix Vercel build (changed removed output hybrid property to static)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel({
     maxDuration: 300
   }),
